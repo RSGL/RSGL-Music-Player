dfe8e938a8182df353393ae1fd328a4e06e77981
Colleague Riley <50218871+ColleagueRiley@users.noreply.github.com>
2021-07-09 21:01:47 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,30 +1,54 @@
 #include "../include/RSGL/RSGL.hpp"
 #include <vector>
 #include <iostream>
+#include <sys/syscall.h>
+
 bool running = true;
 std::vector<RSGL::music> songs;
 std::vector<int> played;
 
+std::vector<RSGL::music> loadFolder(std::string dir){
+    std::vector<RSGL::music> output;
+    std::string c = "find " + dir + " -name \"*.mp3\"";
+    FILE* f = popen(c.data(),"r"); std::string read; char* fs; char ch; int len;
+    if (f){
+        while(fgetc(f) != EOF) len++; fs = (char*) malloc(len);
+        int i =0; 
+        while( ch=fgetc(f) != EOF){ fs[i] = ch; i++; }
+        std::cout << fs;
+        fclose (f);
+    
+        if (fs){
+            std::string file;
+            for (int i=0; i < len; i++){
+                if (fs[i] != '\n') file+= fs[i];
+                else{output.insert(output.end(), RSGL::loadMusic(file)); file="";}
+            }
+            return output; 
+        } 
+    }
+    return {};
+}
+
 int main(){
     RSGL::init();
-    RSGLWindow window = createWindow("name",500,500,500,500, {255,255,255});    
+    RSGLWindow window = createWindow("RSGL Music Player",500,500,300,300, {255,255,255});    
     Event e;
 
     int I=0;  int pause = 0;
     int song = 0; int suffle=0;
-    int dir = 1; int loop = 1;
-    std::string dirr = "~/music/.music/(Probably) Racist/";
+    int dir = 1;
+    /*cAudio::IAudioManager* audioMgr = cAudio::createAudioManager(false);
+    cAudio::IAudioSource* mysound = audioMgr->create("song", "/home/manjaro/Music/.music/(Probably) Racist/22. 4 My Nuckas.mp3",true);*/
+    //songs = {RSGL::loadMusic("/home/manjaro/Music/.music/(Probably) Racist/22. 4 My Nuckas.mp3"),RSGL::loadMusic("/home/manjaro/Music/.music/(Probably) Racist/4. My Shit.mp3"),RSGL::loadMusic("/home/manjaro/Music/.music/Black Man Of Steal/28. Fly Like Me.mp3")};
+    songs = loadFolder("/home/manjaro/Music/.music/");
 
-    songs = {RSGL::loadMusic(dirr+"4. My Shit.mp3"),RSGL::loadMusic(dirr+"22. 4 My Nuckas.mp3"),RSGL::loadMusic(dirr+"17. Only 17.mp3")};
-    
-    RSGL::imgButton Pause = {RSGL::loadImage("../res/pause.png",{200,400,60,60})}; 
-    RSGL::imgButton Skip = {RSGL::loadImage("../res/skip.png",{260,400,60,60})}; 
-    RSGL::imgButton Back = {RSGL::loadImage("../res/back.png",{140,400,60,60})}; 
-    RSGL::imgButton Shuffle = {RSGL::loadImage("../res/shuffle.png",{240,370,30,30})}; 
-    RSGL::imgButton Loop = {RSGL::loadImage("../res/loop.png",{200,370,30,30})}; 
-
+    RSGL::imgButton Pause = {RSGL::loadImage("../res/pause.png",{100,200,60,60})}; 
+    RSGL::imgButton Skip = {RSGL::loadImage("../res/skip.png",{160,200,60,60})}; 
+    RSGL::imgButton Back = {RSGL::loadImage("../res/back.png",{40,200,60,60})}; 
+    RSGL::imgButton Shuffle = {RSGL::loadImage("../res/shuffle.png",{140,170,30,30})}; 
+    RSGL::imgButton Loop = {RSGL::loadImage("../res/loop.png",{100,170,30,30})};
     while (running){
-        RSGL::playMusic(songs[0],1);
         srand(time(NULL));
         e.CheckEvents();
         RSGL::clear();
@@ -34,10 +58,14 @@ int main(){
         Back.draw();
         Shuffle.draw();
         Loop.draw();
+        switch (e.type){
+            case RSGL::quit: running = false; break;
+            case RSGL::KeyPressed: /*mysound->play();*/ break;
+            default: break;
+        }
 
-        if (e.type == RSGL::quit) running = false;
         if (I < songs.size() && !RSGL::isPlaying(songs[song])){
-            //I+=dir; RSGL::playMusic(songs[I],loop); dir=1;
+            RSGL::playMusic(songs[I],1);  I+=dir; if(dir) dir=1;
         } 
         song = I;
         if (suffle){ 
@@ -51,13 +79,13 @@ int main(){
         }
 
         if (Pause.isClicked()){
-            if (pause){RSGL::resumeMusic(); pause=0; Pause.img = RSGL::loadImage("../res/play.png",{200,400,60,60});}
-            if (!pause){RSGL::pauseMusic(); pause=1; Pause.img = RSGL::loadImage("../res/pause.png",{200,400,60,60});}
+            if (pause){RSGL::resumeMusic(); pause=0; /*Pause.img = RSGL::loadImage("../res/play.png",{100,200,60,60});*/}
+            if (!pause){RSGL::pauseMusic(); pause=1; /*Pause.img = RSGL::loadImage("../res/pause.png",{100,200,60,60});*/}
         }
         if (Skip.isClicked()){ RSGL::stopMusic(); dir=1;}
         if (Back.isClicked()){RSGL::stopMusic(); dir=-1;}
         if (Shuffle.isClicked()){ if(suffle) suffle=0; else suffle=1; }
-        if (Loop.isClicked()){ if(loop == 1) loop=-1; else loop=1; }
+        if (Loop.isClicked()){ if(dir) dir=0; else dir=1; }
         
     }
     RSGL::Quit();
